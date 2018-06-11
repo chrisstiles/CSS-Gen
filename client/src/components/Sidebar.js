eb6d0782464ee5f2998c7b963532725a4473db04
Chris Stiles <chrisstiles92@gmail.com>
2018-06-11 00:44:59 -0700

    Make top sidebar title fixed

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -4,8 +4,8 @@ import CodeOutput from './CodeOutput';
 const Sidebar = ({ property, children, outputCSS, outputPreviewStyles, previewCSS, browserPrefixes }) => {
   return (
     <div id="sidebar">
+    <div className="sidebar-title">Controls</div>
     	<div id="sidebar-controls">
-    		<div className="sidebar-title">Controls</div>
       	{children}
       </div>
       <CodeOutput
