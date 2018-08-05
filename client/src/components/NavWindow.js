6742b643e3a9bb5ca60842347f29f8171af2d194
Chris Stiles <chrisstiles92@gmail.com>
2018-08-05 15:58:42 -0700

    Remove prebuild code from logo

--- client/src/components/NavWindow.js
+++ client/src/components/NavWindow.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import Logo from '-!react-svg-loader?name=Logo!../images/logo.svg';
+import Logo from '../images/logo.svg';
 
 const NavWindow = () => {
 	return (
