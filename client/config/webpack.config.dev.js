5503cae0858acce4770571b809259967cfd9bd88
Chris Stiles <chrisstiles92@gmail.com>
2018-08-05 13:41:30 -0700

    Style navigation window

--- client/config/webpack.config.dev.js
+++ client/config/webpack.config.dev.js
@@ -140,12 +140,13 @@ module.exports = {
           },
           {
             test: /\.svg$/,
+            issuer: /\.jsx?$/,
             use: [
               {
                 loader: "babel-loader"
               },
               {
-                loader: "react-svg-loader",
+                loader: "react-svg-loader"
                 options: {
                   jsx: true // true outputs JSX tags
                 }
--- client/src/components/NavWindow.js
+++ client/src/components/NavWindow.js
@@ -9,10 +9,12 @@ const NavWindow = () => {
 				<Logo />
 			</NavLink>
 
-			<NavLink activeClassName="current" exact to="/">Home</NavLink><br />
-			<NavLink activeClassName="current" to="/box-shadow-generator">Box Shadow</NavLink><br />
-			<NavLink activeClassName="current" to="/border-radius-generator">Border Radius</NavLink><br />
-      <NavLink activeClassName="current" to="/gradient-generator">Gradient</NavLink>
+			<div id="page-links">
+				<h2>CSS Generators</h2>
+				<NavLink activeClassName="current" className="box-shadow" to="/box-shadow-generator">Box Shadow</NavLink>
+				<NavLink activeClassName="current" className="border-radius" to="/border-radius-generator">Border Radius</NavLink>
+	      <NavLink activeClassName="current" className="gradient" to="/gradient-generator">Gradient</NavLink>
+      </div>
 		</div>
 	);
 }
