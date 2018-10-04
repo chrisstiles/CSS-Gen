017779fbd8e4068124b635ac5ee091aa667516ba
chrisstiles <chrisstiles92@gmail.com>
2018-10-04 10:04:29 -0700

    Improve text shadow input styles

--- client/src/components/generators/previews/StaticWindowPreview.js
+++ client/src/components/generators/previews/StaticWindowPreview.js
@@ -4,9 +4,14 @@ class StaticWindowPreview extends React.Component {
 	render() {
 		const { isDefault, backgroundColor, children } = this.props;
 		const wrapperStyle = isDefault ? {} : { backgroundColor };
+		const className = backgroundColor === 'transparent' ? 'no-bg' : 'has-bg';
 
 		return (
-			<div id="static-preview" style={wrapperStyle}>
+			<div 
+				id="static-preview" 
+				style={wrapperStyle}
+				className={className}
+			>
 				{!isDefault ? children : 
 					<div id="checker-window">
 						<div 
