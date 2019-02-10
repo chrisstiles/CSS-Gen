85be6daa1104fddbabb4ef4cf2547eb96e39d006
Chris Stiles <chrisstiles92@gmail.com>
2019-02-10 14:09:57 -0800

    Improve performance when persisting state

--- client/src/components/Generator.js
+++ client/src/components/Generator.js
@@ -4,50 +4,44 @@ import Sidebar from './Sidebar';
 import BottomContent from './BottomContent';
 import LoadingSpinner from './LoadingSpinner';
 import { setLoading } from '../util/helpers';
-import _ from 'underscore';
 
 class Generator extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { 
-      keys: ''
-    };
-
-    this.handleKeydown = this.handleKeydown.bind(this);
-    this.handleKeyup = this.handleKeyup.bind(this);
     this.renderPresets = this.renderPresets.bind(this);
     this.renderToolbar = this.renderToolbar.bind(this);
-
-    var loggedLocalStorageError = false;
+    this.renderPreviewSettings = this.renderPreviewSettings.bind(this);
 
     // Persist generator state to local storage
-    this.persistState = _.debounce(newProps => {
-      if (window.localStorage) {
-        const path = window.location.pathname;
-        const { generatorState, previewState } = newProps;
-        const state = { generatorState, previewState }
+    let loggedLocalStorageError = false;
+    let persistStateTimer;
 
-        // Store in localStorage if there is enough space
-        try {
-          window.localStorage.setItem(path, JSON.stringify(state));
-          loggedLocalStorageError = false;
-        } catch(e) {
-          if (e.code === 22 && !loggedLocalStorageError) {
-            loggedLocalStorageError = true;
-            console.log('Data not persisted, exceeds localStorage quota');
+    this.persistState = () => {
+      // Use a timer to prevent this from being called too often
+      clearTimeout(persistStateTimer);
+      persistStateTimer = setTimeout(() => {
+        if (window.localStorage) {
+          const path = window.location.pathname;
+          const { generatorState, previewState } = this.props;
+          const state = { generatorState, previewState }
+
+          // Store in localStorage if there is enough space
+          try {
+            window.localStorage.setItem(path, JSON.stringify(state));
+            loggedLocalStorageError = false;
+          } catch (e) {
+            if (e.code === 22 && !loggedLocalStorageError) {
+              loggedLocalStorageError = true;
+              console.log('Data not persisted, exceeds localStorage quota');
+            }
           }
         }
-      }
-    }, 300);
+      }, 500);
+    }
   }
 
   componentDidMount() {
-    // document.addEventListener('keydown', this.handleKeydown, false);
-    // document.addEventListener('keyup', this.handleKeyup, false);
-    // document.addEventListener('keydown', this.props.handleKeydown, false);
-    // document.addEventListener('keyup', this.props.handleKeyup, false);
-
     if (this.props.onWrapperMount) {
       this.props.onWrapperMount(this.generatorWrapper);
     }
@@ -55,43 +49,14 @@ class Generator extends React.Component {
     setLoading(false);
   }
 
-  componentWillUnmount(){
-    // document.removeEventListener('keydown', this.handleKeydown, false);
-    // document.removeEventListener('keyup', this.handleKeyup, false);
-    // document.removeEventListener('keydown', this.props.handleKeydown, false);
-    // document.removeEventListener('keyup', this.props.handleKeyup, false);
+  componentDidUpdate() {
+    this.persistState();
   }
 
-  componentWillReceiveProps(newProps) {
-    // Save state to local storage
-    this.persistState(newProps);
-  }
-
-  handleKeydown(event) {
-    // Adds classes to the generator for common keys pressed
-    var keys = [];
-
-    // Shift key
-    if (event.shiftKey) {
-      keys.push('key-shift');
+  renderPreviewSettings () {
+    if (this.props.renderPreviewSettings) {
+      return this.props.renderPreviewSettings();
     }
-
-    // Command or control keys
-    if (event.metaKey || event.ctrlKey) {
-      keys.push('key-meta');
-    }
-
-    // Alt or option keys
-    if (event.altKey) {
-      keys.push('key-alt');
-    }
-
-    const keyString = keys.length ? ` ${keys.join(' ')}` : '';
-    this.setState({ keys:  keyString});
-  }
-
-  handleKeyup(event) {
-    this.setState({ keys: '' });
   }
 
   renderPresets() {
@@ -150,8 +115,9 @@ class Generator extends React.Component {
               {this.props.renderInputs()}
             </Sidebar>
             <BottomContent 
+              renderPreviewSettings={this.renderPreviewSettings}
+              renderPresets={this.renderPresets}
               renderOutput={renderOutput}
-              renderPresets={this.renderPresets} 
               outputCode={generatorState.css.output}
               outputPreviewStyles={outputPreviewStyles}
               showBrowserPrefixes={showBrowserPrefixes}
