bb54657536e76af1edf2f572a2e8998b460248f7
Chris Stiles <chrisstiles92@gmail.com>
2019-02-10 23:39:39 -0800

    changed NPM_CONFIG_PRODUCTION to false

--- index.js
+++ index.js
@@ -6,16 +6,11 @@ const app = express();
 require('./routes/')(app);
 
 if (process.env.NODE_ENV === 'production') {
-  // Express will serve up production assets
-  // like our main.js file, or main.css file!
-  app.use(express.static('static'));
-
   const path = require('path');
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
-  // Express will serve up the index.html file
-  // if it doesn't recognize the route
-  app.get('/*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 
