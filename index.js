64b58cfa7d9847826940c127c367b5529442a2bd
Chris Stiles <chrisstiles92@gmail.com>
2019-02-12 13:02:48 -0800

    Log for both production and server

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 require('./routes/')(app);
 
 if (process.env.NODE_ENV === 'production') {
-  console.log('This is a production build');
+  console.log('PRODUCTION BUILD');
 
   const path = require('path');
   
@@ -14,6 +14,8 @@ if (process.env.NODE_ENV === 'production') {
   app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
+} else {
+  console.log('DEVELOPMENT BUILD');
 }
 
 const PORT = process.env.PORT || 5000;
