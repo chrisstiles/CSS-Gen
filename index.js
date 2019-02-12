b2f9e90f2a78df691d9859eb6bc4a152f8a26dbb
Chris Stiles <chrisstiles92@gmail.com>
2019-02-11 21:44:16 -0800

    Change asset paths...again

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require('express');
-
 const app = express();
 
 // API Routes
 require('./routes/')(app);
 
 if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
   const path = require('path');
-  app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
