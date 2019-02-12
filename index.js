2d2855c60e1ea9c5fe9bf94e31744a31f5738080
Chris Stiles <chrisstiles92@gmail.com>
2019-02-11 21:27:32 -0800

    Change path to react router

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ require('./routes/')(app);
 
 if (process.env.NODE_ENV === 'production') {
   const path = require('path');
-  app.use(express.static(path.join(__dirname, 'client/build/static')));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
-  app.get('*', (req, res) => {
+  app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
