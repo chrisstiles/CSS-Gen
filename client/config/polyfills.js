8cc909dcba45b164aaaf5a2d268dd8595483b5de
chrisstiles <chrisstiles92@gmail.com>
2019-02-12 10:33:54 -0800

    Add String.prototype.includes polyfill for IE.

--- client/config/polyfills.js
+++ client/config/polyfills.js
@@ -20,3 +20,19 @@ Object.assign = require('object-assign');
 if (process.env.NODE_ENV === 'test') {
   require('raf').polyfill(global);
 }
+
+if (!String.prototype.includes) {
+  Object.defineProperty(String.prototype, 'includes', {
+    value: function (search, start) {
+      if (typeof start !== 'number') {
+        start = 0
+      }
+
+      if (start + search.length > this.length) {
+        return false
+      } else {
+        return this.indexOf(search, start) !== -1
+      }
+    }
+  })
+}
