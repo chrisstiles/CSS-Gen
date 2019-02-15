5e440e213cf6f3995fd94d6d3df4e23c2466d6f7
Chris Stiles <chrisstiles92@gmail.com>
2019-02-14 21:58:13 -0800

    Use functional component

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import _ from 'underscore';
 
-class Sidebar extends React.Component {
-  render() {
-    return (
-      <div id="sidebar">
-        <div className="sidebar-title">Controls</div>
-        <div id="sidebar-controls">
-          {this.props.children}
-        </div>
+const Sidebar = props => {
+  return (
+    <div id="sidebar">
+      <div className="sidebar-title">Controls</div>
+      <div id="sidebar-controls">
+        {props.children}
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Sidebar;
