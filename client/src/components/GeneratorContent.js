7e7f0fa86da3993c1abe07dd8e05943935017303
chrisstiles <chrisstiles92@gmail.com>
2019-03-11 16:46:14 -0700

    Begin adding functionality to fix code output position

--- client/src/components/GeneratorContent.js
+++ client/src/components/GeneratorContent.js
@@ -2,9 +2,15 @@ import React from 'react';
 import CodeOutput from './CodeOutput';
 import { isArray } from 'underscore';
 
-class GeneratorContent extends React.PureComponent {
+class GeneratorOutput extends React.PureComponent {
+  calculateScrollPoints = () => {
+    if (!this.wrapper) return;
+
+    
+  }
+
   render() {
-    let { output, children } = this.props;
+    let { output } = this.props;
     if (!isArray(output)) output = [output];
 
     const codeViewers = output.map(({ language, code }) => {
@@ -21,11 +27,22 @@ class GeneratorContent extends React.PureComponent {
     if (output.length > 1) outputWrapperProps.className = 'multiple';
 
     return (
+      <div 
+        ref={wrapper => { this.wrapper = wrapper }}
+        {...outputWrapperProps}
+      >
+        {codeViewers}
+      </div>
+    );
+  }
+}
+
+class GeneratorContent extends React.PureComponent {
+  render() {
+    return (
       <div id="generator-content">
-        <div {...outputWrapperProps}>
-          {codeViewers}
-        </div>
-        {children}
+        <GeneratorOutput output={this.props.output} />
+        {this.props.children}
       </div>
     );
   }
