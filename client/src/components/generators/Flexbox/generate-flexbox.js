b0c222702290d2559a12fb3d4cf984c424960610
chrisstiles <chrisstiles92@gmail.com>
2019-04-16 11:22:04 -0700

    Continue working on grouping similar styles in flexbox output

--- client/src/components/generators/Flexbox/generate-flexbox.js
+++ client/src/components/generators/Flexbox/generate-flexbox.js
@@ -14,35 +14,113 @@ export default function generate({ containerStyles, itemStyles, childElements },
   const itemCSS = generateCSSString(itemStyles, '.item', defaultState.itemStyles);
   if (itemCSS) css.push(itemCSS);
 
+  const uniqueItemStyles = [];
+  const uniqueItemSelectors = [];
+  const uniqueItemValues = [];
+
+  const uniqueStyles = {};
+
   if (childElements.length) {
-    childElements.forEach((child, index) => {
+    childElements.forEach((child, childIndex) => {
       const { id, ...restStyles } = child;
 
-      let hasUniqueValues = false;
-      const childStyles = mapObject({ ...restStyles }, (value, key) => {
+      let selector;
+
+      if (childIndex === 0) {
+        selector = `.item:first-child`;
+      } else if (childIndex === childElements.length - 1) {
+        selector = `.item:last-child`;
+      } else {
+        selector = `.item:nth-child(${childIndex + 1})`;
+      }
+
+      // let hasUniqueValues = false;
+      const uniqueValues = [];
+
+      const childStyles = mapObject({ ...restStyles }, (_value, key) => {
         // We only output items for individual items
         // if their values are different than the default
-        const newValue = itemStyles[key] === value ? null : value;
-        if (newValue) hasUniqueValues = true;
-        return newValue;
-      });
+        const value = itemStyles[key] === _value ? null : _value;
 
-      if (hasUniqueValues) {
-        let selector;
+        if (value) {
+          if (!uniqueStyles.hasOwnProperty(key)) {
+            uniqueStyles[key] = {
+              styles: [{ selector, value }],
+              values: [value]
+            }
+          } else {
+            const data = uniqueStyles[key];
+            // const valueIndex = styles.values.indexOf(value);
 
-        if (index === 0) {
-          selector = `.item:first-child`;
-        } else if (index === childElements.length - 1) {
-          selector = `.item:last-child`;
-        } else {
-          selector = `.item:nth-child(${index + 1})`;
+            // Another child item uses this same style
+            if (data.values.includes(value)) {
+              data.styles = data.styles.map(style => {
+                if (style.value === value) style.selector += `,\n${selector}`;
+                return style;
+              });
+            } else {
+              data.values.push(value);
+              data.styles.push({ selector, value: value });
+            }
+          }
+
+
+          const style = {};
+          style[key] = value;
+
+          uniqueValues.push(style);
+          // hasUniqueValues = true;
+          // console.log(key, newValue);
         }
 
-        css.push(generateCSSString(childStyles, selector));
+        return value;
+      });
+
+      // console.log(uniqueValues)
+
+      // if (hasUniqueValues) {
+      // This item has some different css than the default item styles
+      if (uniqueValues.length) {
+        // let selector;
+
+        // if (childIndex === 0) {
+        //   selector = `.item:first-child`;
+        // } else if (childIndex === childElements.length - 1) {
+        //   selector = `.item:last-child`;
+        // } else {
+        //   selector = `.item:nth-child(${childIndex + 1})`;
+        // }
+
+        // Check if this item's styles are the same as any other 
+        // child so we can combine them in the output CSS
+        const styles = JSON.stringify(uniqueValues);
+        const styleIndex = uniqueItemStyles.indexOf(styles);
+
+        if (styleIndex === -1) {
+          // These styles are not shared by another child item
+          uniqueItemStyles.push(styles);
+          uniqueItemSelectors.push(selector);
+          uniqueItemValues.push(childStyles);
+        } else {
+          // Another child item uses the same styles, so combine the selectors
+          uniqueItemSelectors[styleIndex] = `${uniqueItemSelectors[styleIndex]},\n${selector}`;
+        }
+
+
+
+        // css.push(generateCSSString(childStyles, selector));
       }
 
       html.push('  <div class="item"></div>');
     });
+
+    console.log(uniqueStyles)
+
+    if (uniqueItemSelectors.length) {
+      uniqueItemSelectors.forEach((selector, index) => {
+        css.push(generateCSSString(uniqueItemValues[index], selector));
+      });
+    }
 
     html.unshift('<div class="container">');
     html.push('</div>');
